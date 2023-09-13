2763c7f5866b2d6abd69c1003a6ded52540969a8
João Vitor Espig <jotinha1300@gmail.com>
2023-09-12 22:18:53 -0300

    feat(user): added FindUserById

--- pkg/models/user/user_dao.go
+++ pkg/models/user/user_dao.go
@@ -1,1 +1,19 @@
+// package user provides support for operations with User model
 package user
+
+import "chronos/config"
+
+var (
+	db                = config.DB
+	findUserByIdQuery = `SELECT "id", "username" FROM "user" WHERE "id" = ?;`
+)
+
+func FindUserByID(id uint) *User {
+	u := &User{}
+	err := db.QueryRow(findUserByIdQuery, id).Scan(&u.ID, &u.Username)
+	if err != nil {
+		return nil
+	}
+
+	return u
+}
