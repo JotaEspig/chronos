a771900653970226946a9f390249e36edf47bab8
João Vitor Espig <jotinha1300@gmail.com>
2023-09-12 23:48:31 -0300

    test(user_test.go): fixed tests

--- tests/user_dao_test.go
+++ tests/user_dao_test.go
@@ -5,17 +5,26 @@ import (
 	"chronos/pkg/models/user"
 	"testing"
 
+	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
 )
 
 func TestFindUserByID(t *testing.T) {
+	godotenv.Load("../.env")
+	config.InitDB()
+
 	tx, err := config.DB.Begin()
 	assert.Equal(t, nil, err)
 	defer tx.Rollback()
 
 	cleanDB(tx)
-	tx.Exec("INSERT INTO \"user\" VALUES (1, 'test')")
-	u := user.FindUserByID(1)
+	_, err = tx.Exec("INSERT INTO \"user\" VALUES (1, 'test');")
+	assert.Equal(t, nil, err)
+
+	u := user.FindUserByID(tx, 1)
 	assert.NotEqual(t, nil, u)
+	assert.Equal(t, uint(1), u.ID)
+	assert.Equal(t, "test", u.Username)
+
 	return
 }
