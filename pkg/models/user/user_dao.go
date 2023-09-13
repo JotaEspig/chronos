6c026e0f5d20885f3de9927fff62a5207438e35c
João Vitor Espig <jotinha1300@gmail.com>
2023-09-12 23:49:05 -0300

    refactor(user_dao.go): using transaction instead of DB directly

--- pkg/models/user/user_dao.go
+++ pkg/models/user/user_dao.go
@@ -1,16 +1,15 @@
 // package user provides support for operations with User model
 package user
 
-import "chronos/config"
+import "database/sql"
 
 var (
-	db                = config.DB
 	findUserByIdQuery = `SELECT "id", "username" FROM "user" WHERE "id" = ?;`
 )
 
-func FindUserByID(id uint) *User {
+func FindUserByID(tx *sql.Tx, id uint) *User {
 	u := &User{}
-	err := db.QueryRow(findUserByIdQuery, id).Scan(&u.ID, &u.Username)
+	err := tx.QueryRow(findUserByIdQuery, id).Scan(&u.ID, &u.Username)
 	if err != nil {
 		return nil
 	}
