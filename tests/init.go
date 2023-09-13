a0dbb36d39ca379ccceab23309324525f7c8c5ca
João Vitor Espig <jotinha1300@gmail.com>
2023-09-13 00:43:17 -0300

    test: added new test for user_dao

--- tests/init.go
+++ tests/init.go
@@ -1,9 +1,14 @@
 package tests
 
 import (
+	"chronos/config"
 	"os"
+
+	"github.com/joho/godotenv"
 )
 
 func init() {
+	godotenv.Load("../.env")
+	config.InitDB()
 	os.Setenv("IS_TEST_RUN", "true")
 }
--- tests/user_dao_test.go
+++ tests/user_dao_test.go
@@ -5,26 +5,68 @@ import (
 	"chronos/pkg/models/user"
 	"testing"
 
-	"github.com/joho/godotenv"
 	"github.com/stretchr/testify/assert"
 )
 
-func TestFindUserByID(t *testing.T) {
-	godotenv.Load("../.env")
-	config.InitDB()
-
+func TestCreateUser(t *testing.T) {
 	tx, err := config.DB.Begin()
 	assert.Equal(t, nil, err)
 	defer tx.Rollback()
 
 	cleanDB(tx)
+
+	newUser := &user.User{
+		Username: "newuser",
+	}
+
+	err = user.CreateUser(tx, newUser)
+	assert.Equal(t, nil, err)
+
+	// Check that the user has been successfully created and has a non-zero ID.
+	assert.NotEqual(t, uint(0), newUser.ID)
+
+	// Fetch the user from the database by ID and check if it matches the created user.
+	var id uint
+	var username string
+
+	tx.QueryRow("SELECT \"id\", \"username\" FROM \"user\";").Scan(&id, &username)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, newUser.ID, id)
+	assert.Equal(t, newUser.Username, username)
+}
+
+func TestFindUserByID(t *testing.T) {
+	tx, err := config.DB.Begin()
+	assert.Equal(t, nil, err)
+	defer tx.Rollback()
+
+	cleanDB(tx)
+
+	// Inserts a user in the database
 	_, err = tx.Exec("INSERT INTO \"user\" VALUES (1, 'test');")
 	assert.Equal(t, nil, err)
 
-	u := user.FindUserByID(tx, 1)
+	// Try to fetch the user
+	u, err := user.FindUserByID(tx, 1)
 	assert.NotEqual(t, nil, u)
+	assert.Equal(t, nil, err)
 	assert.Equal(t, uint(1), u.ID)
 	assert.Equal(t, "test", u.Username)
+}
 
-	return
+func TestFindUserByUsername(t *testing.T) {
+	tx, err := config.DB.Begin()
+	assert.Equal(t, nil, err)
+	defer tx.Rollback()
+
+	cleanDB(tx)
+
+	_, err = tx.Exec("INSERT INTO \"user\" VALUES (1, 'test');")
+	assert.Equal(t, nil, err)
+
+	u, err := user.FindUserByUsername(tx, "test")
+	assert.NotEqual(t, nil, u)
+	assert.Equal(t, nil, err)
+	assert.Equal(t, uint(1), u.ID)
+	assert.Equal(t, "test", u.Username)
 }
