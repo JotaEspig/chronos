e36e2f920ff9cf68622ed3a59459710ed6a01651
João Vitor Espig <jotaespig@gmail.com>
2023-09-17 20:46:06 -0300

    docs(userapi/user.go): added comments to document the functions

--- pkg/api/userapi/user.go
+++ pkg/api/userapi/user.go
@@ -10,6 +10,8 @@ import (
 	"github.com/labstack/echo/v4"
 )
 
+// createUser is a user controller that receives a JSON in the body of the
+// request and return a status code
 func createUser(c echo.Context) error {
 	u := user.User{}
 	err := json.NewDecoder(c.Request().Body).Decode(&u)
@@ -31,6 +33,8 @@ func createUser(c echo.Context) error {
 	return c.NoContent(http.StatusCreated)
 }
 
+// getUser is a user controller that receives a param ("id") in the url path
+// and return a JSON if succeeds or a status code if something went wrong
 func getUser(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
@@ -51,6 +55,12 @@ func getUser(c echo.Context) error {
 	return c.JSON(http.StatusOK, uMap)
 }
 
+// updateUser is a user controller that receives a param ("id") in the url path
+// and a JSON in the body of the request and return a status code.
+// Attention: You must send the whole user values even if you don't want to
+// update something, e.g. you want to update just the age of the user, even so
+// you must include the original value in the JSON that contains the user.
+// That's because of the way UpdateUser function works
 func updateUser(c echo.Context) error {
 	u := user.User{}
 	err := json.NewDecoder(c.Request().Body).Decode(&u)
@@ -77,6 +87,8 @@ func updateUser(c echo.Context) error {
 	return c.NoContent(http.StatusOK)
 }
 
+// deleteUser is a user controller that receives a param ("id") in the url path
+// and a JSON in the body of the request and return a status code
 func deleteUser(c echo.Context) error {
 	id, err := strconv.Atoi(c.Param("id"))
 	if err != nil {
