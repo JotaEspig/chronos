34868c84211c4fe1165f2f81fb150318cb613712
João Vitor Espig <jotaespig@gmail.com>
2023-09-25 18:10:08 -0300

    feat(api/routes.go): appending AvailablesRoutes from employeeapi

--- pkg/api/routes.go
+++ pkg/api/routes.go
@@ -1,6 +1,7 @@
 package api
 
 import (
+	"chronos/pkg/api/employeeapi"
 	"chronos/pkg/api/other"
 	"chronos/pkg/api/userapi"
 	"chronos/pkg/types"
@@ -11,4 +12,5 @@ var AllAvailableRoutes []types.Route
 func init() {
 	AllAvailableRoutes = append(AllAvailableRoutes, other.AvailableRoutes...)
 	AllAvailableRoutes = append(AllAvailableRoutes, userapi.AvailableRoutes...)
+	AllAvailableRoutes = append(AllAvailableRoutes, employeeapi.AvailableRoutes...)
 }
