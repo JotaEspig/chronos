04c472718e9b3166be7b60e88f782cc794348fcc
João Vitor Espig <jotaespig@gmail.com>
2023-09-26 12:34:14 -0300

    feat: added function Sanitize for each model

--- pkg/models/scheduling/scheduling.go
+++ pkg/models/scheduling/scheduling.go
@@ -1,7 +1,11 @@
 // package scheduling provides support for operations with Scheduling model
 package scheduling
 
-import "chronos/pkg/types"
+import (
+	"chronos/pkg/types"
+
+	"github.com/microcosm-cc/bluemonday"
+)
 
 type Scheduling struct {
 	ID     uint   `json:"id"`
@@ -9,6 +13,11 @@ type Scheduling struct {
 	End    string `json:"end"`
 	UserID uint   `json:"user_id"`
 	TimeID uint   `json:"time_id"`
+}
+
+func (s *Scheduling) Sanitize(policy *bluemonday.Policy) {
+	s.Start = policy.Sanitize(s.Start)
+	s.End = policy.Sanitize(s.End)
 }
 
 func (s *Scheduling) ToMap() types.JsonMap {
