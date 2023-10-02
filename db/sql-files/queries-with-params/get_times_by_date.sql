bfec9b2dee4b5608954919f79f40e9dc25ca6a04
João Vitor Espig <jotaespig@gmail.com>
2023-10-02 00:29:08 -0300

    fix(get_times_by_data.sql): trying new conditions
    now it seems more correct

--- db/sql-files/queries-with-params/get_times_by_date.sql
+++ db/sql-files/queries-with-params/get_times_by_date.sql
@@ -1,3 +1,11 @@
+-- ("repeat" & 64 AND ((unixepoch("start") - unixepoch(?)) / (86400)) == 7)
+-- statement checks if date is 7 days after "start"
+-- Notes:
+--    86400 is the amount of seconds in 7 days
+--    ? will be replaced by an arbitrary date
+--    %d will be replaced by amount of elements per page
+--    second ? will be replaced by the page * amount of elements per page
 SELECT * FROM "time"
-WHERE "start" >= ?
+WHERE "repeat" & 32 == 32
+    OR ("repeat" & 64 AND ((unixepoch("start") - unixepoch(?)) / (86400)) == 7)
 LIMIT %d OFFSET ?;
