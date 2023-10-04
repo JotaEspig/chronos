7a38c3b4abe4941eec2ae0ae44310d5528be0702
gustavo <mrgustavotramontin@gmail.com>
2023-10-04 15:38:34 -0300

    feat(front): get shedules from api

--- chronos-files/js/main.js
+++ chronos-files/js/main.js
@@ -34,9 +34,45 @@ function render_schedules() {
   }
 }
 
-add_schedule(2, 2, "seg", "free")
-add_schedule(0, 3, "ter", "free")
-add_schedule(0, 4, "qua", "free")
-add_schedule(0, 6, "qui", "free")
-add_schedule(0, 1, "sex", "free")
+async function request_schedules(week) {
+	const api = "http://localhost:8080/api/time"
+	const today = new Date(Date.now())
+	const year = today.getFullYear().toString()
+	const month = (today.getMonth() + 1).toLocaleString('en-US', {
+    minimumIntegerDigits: 2,
+    useGrouping: false })
+
+	const week_inc = ['seg', 'ter', 'qua', 'qui', 'sex'].indexOf(week) + 1
+
+	const day = (today.getDay() + week_inc).toLocaleString('en-US', {
+    minimumIntegerDigits: 2,
+    useGrouping: false })
+
+	const date = `${year}-${month}-${day}`
+	console.log(date)
+
+	const req = await fetch(`${api}?date=${date}&page=0`)
+
+	const json = await req.json()
+	console.log(json)
+	
+	json.forEach(t => {
+		const start = (new Date(t.start)).getHours() - 9
+		const end = (new Date(t.end)).getHours() - 9
+		const duration = end - start
+		add_schedule(start,duration, week, "free")
+	})
+
+}
+
+(async function() {
+
+await request_schedules("seg")
+await request_schedules("ter")
+await request_schedules("qua")
+await request_schedules("qui")
+await request_schedules("sex")
+console.log(state)
+
 render_schedules()
+})()
