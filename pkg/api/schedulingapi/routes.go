40900a193e5159bdd5fa396c846595962a0d427a
João Vitor Espig <jotinha1300@gmail.com>
2023-10-04 14:06:54 -0300

    feat: added get schedulings by date

--- pkg/api/schedulingapi/routes.go
+++ pkg/api/schedulingapi/routes.go
@@ -5,6 +5,7 @@ import "chronos/pkg/types"
 var AvailableRoutes []types.Route = []types.Route{
 	{Path: "/api/scheduling/add", Method: types.MethodPOST, Fn: createScheduling},
 	{Path: "/api/scheduling/:id", Method: types.MethodGET, Fn: getScheduling},
+	{Path: "/api/scheduling", Method: types.MethodGET, Fn: getSchedulingsByDate},
 	{Path: "/api/scheduling/:id", Method: types.MethodPUT, Fn: updateScheduling},
 	{Path: "/api/scheduling/:id", Method: types.MethodDELETE, Fn: deleteScheduling},
 }
