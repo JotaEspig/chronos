47a5faf571b35e62c94a8d4a9a94733e1e74cfab
João Vitor Espig <jotaespig@gmail.com>
2023-10-12 01:15:06 -0300

    style: break line

--- pkg/api/other/routes.go
+++ pkg/api/other/routes.go
@@ -9,5 +9,6 @@ import (
 
 var AvailableRoutes []types.Route = []types.Route{
 	{Path: "/api/hello", Method: types.MethodGET, Fn: hello},
-	{Path: "/api/jwt", Method: types.MethodGET, Fn: needsJWT, Middlewares: []echo.MiddlewareFunc{config.JWTMiddleware}},
+	{Path: "/api/jwt", Method: types.MethodGET, Fn: needsJWT,
+		Middlewares: []echo.MiddlewareFunc{config.JWTMiddleware}},
 }
