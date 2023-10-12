2b5de9f23a841797ec0a453b5586f431a155074c
João Vitor Espig <jotaespig@gmail.com>
2023-10-12 00:48:58 -0300

    test: added a test route for test JWT

--- pkg/api/other/routes.go
+++ pkg/api/other/routes.go
@@ -1,7 +1,13 @@
 package other
 
-import "chronos/pkg/types"
+import (
+	"chronos/config"
+	"chronos/pkg/types"
+
+	"github.com/labstack/echo/v4"
+)
 
 var AvailableRoutes []types.Route = []types.Route{
 	{Path: "/api/hello", Method: types.MethodGET, Fn: hello},
+	{Path: "/api/jwt", Method: types.MethodGET, Fn: needsJWT, Middlewares: []echo.MiddlewareFunc{config.JWTMiddleware}},
 }
