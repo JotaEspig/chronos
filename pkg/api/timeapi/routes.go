64230284ef522d608bb7cb08552b4347fd513faa
João Vitor Espig <jotinha1300@gmail.com>
2023-10-18 14:55:25 -0300

    feat(timeapi): added authentication

--- pkg/api/timeapi/routes.go
+++ pkg/api/timeapi/routes.go
@@ -1,11 +1,21 @@
 package timeapi
 
-import "chronos/pkg/types"
+import (
+	"chronos/config"
+	"chronos/pkg/types"
+
+	"github.com/labstack/echo/v4"
+)
 
 var AvailableRoutes []types.Route = []types.Route{
-	{Path: "/api/time/add", Method: types.MethodPOST, Fn: createTime},
-	{Path: "/api/time/:id", Method: types.MethodGET, Fn: getTime},
-	{Path: "/api/time", Method: types.MethodGET, Fn: getTimesByDate},
-	{Path: "/api/time/:id", Method: types.MethodPUT, Fn: updateTime},
-	{Path: "/api/time/:id", Method: types.MethodDELETE, Fn: deleteTime},
+	{Path: "/api/time/add", Method: types.MethodPOST, Fn: createTime,
+		Middlewares: []echo.MiddlewareFunc{config.JWTMiddleware()}},
+	{Path: "/api/time/:id", Method: types.MethodGET, Fn: getTime,
+		Middlewares: []echo.MiddlewareFunc{config.JWTMiddleware()}},
+	{Path: "/api/time", Method: types.MethodGET, Fn: getTimesByDate,
+		Middlewares: []echo.MiddlewareFunc{config.JWTMiddleware()}},
+	{Path: "/api/time/:id", Method: types.MethodPUT, Fn: updateTime,
+		Middlewares: []echo.MiddlewareFunc{config.JWTMiddleware()}},
+	{Path: "/api/time/:id", Method: types.MethodDELETE, Fn: deleteTime,
+		Middlewares: []echo.MiddlewareFunc{config.JWTMiddleware()}},
 }
