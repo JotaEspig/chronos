7b1b2c2dd1b5ca04dc3f07fd0ddacc63a049aa61
gustavo <mrgustavotramontin@gmail.com>
2023-10-25 15:39:35 -0300

    fix(front): send authorization tokens

--- chronos-files/js/main.js
+++ chronos-files/js/main.js
@@ -59,11 +59,21 @@ async function request_schedules(offset, forward=true) {
 	const date = `${year}-${month}-${day}`
 	console.log(date)
 
-	const req = await fetch(`${api}?date=${date}&page=0`).catch(handle_error)
+	const cookies = document.cookie.split(";").map(x => x.split('='))
+	const token = cookies.find(x => x[0] == "jwt")[1]
+	const req = await fetch(`${api}?date=${date}&page=0`, {
+		headers: {
+			"Authorization": "Bearer " + token
+		}
+	}).catch(handle_error)
+
 
 	const json = await req.json()
 	console.log(json)
 	
+	if (json.message)
+		window.location = "/login.html"
+
 	json.forEach(t => {
 		const start = (new Date(t.start)).getHours() + (new Date(t.start)).getMinutes()/60 - 9
 		const end = (new Date(t.end)).getHours() + (new Date(t.end)).getMinutes()/60 - 9 
