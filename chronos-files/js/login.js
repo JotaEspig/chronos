d7f52331febd01492e94e90b2827741a5e8e106a
João Vitor Espig <jotinha1300@gmail.com>
2023-11-08 14:41:30 -0300

    fix(frontend): somethings

--- chronos-files/js/login.js
+++ chronos-files/js/login.js
@@ -1,26 +1,23 @@
-
 function login(e) {
-  e.preventDefault()
-  console.log(e)
-  const username = document.querySelector("#username").value
-  const password = document.querySelector("#password").value
+    e.preventDefault()
+    console.log(e)
+    const username = document.querySelector("#username").value
+    const password = document.querySelector("#password").value
 
-  const api = "/api/login"
-  fetch(api, {
-    method: "POST",
-    body: JSON.stringify({
-      username: username,
-      password: password
-    })
-  }).then(res => res.json()).then(res => {
-    if (res.message) {
-      alert("failed: " + res.message)
-      return
-    }
-    const token = res.token 
-    console.log(token)
-    document.cookie =`jwt=${token}`
-  })
-
+    const api = "/api/login"
+    fetch(api, {
+        method: "POST",
+        body: JSON.stringify({
+            username: username,
+            password: password
+        })
+    }).then(res => res.json()).then(res => {
+            if (res.message) {
+                alert("failed: " + res.message)
+                return
+            }
+            const token = res.token 
+            document.cookie =`jwt=${token}`
+            window.location = "/"
+        })
 }
-
