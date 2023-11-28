b759819f803b245376d4672b42a3eed0c5c2999a
João Vitor Espig <jotaespig@gmail.com>
2023-11-28 13:59:00 -0300

    style: small changes

--- chronos-files/js/login.js
+++ chronos-files/js/login.js
@@ -1,23 +1,24 @@
 function login(e) {
-    e.preventDefault()
-    console.log(e)
-    const username = document.querySelector("#username").value
-    const password = document.querySelector("#password").value
+    e.preventDefault();
+    console.log(e);
+    const username = document.querySelector("#username").value;
+    const password = document.querySelector("#password").value;
 
-    const api = "/api/login"
+    const api = "/api/login";
     fetch(api, {
         method: "POST",
         body: JSON.stringify({
             username: username,
             password: password
         })
-    }).then(res => res.json()).then(res => {
+    }).then(res => res.json())
+        .then(res => {
             if (res.message) {
-                alert("failed: " + res.message)
-                return
+                alert("failed: " + res.message);
+                return;
             }
-            const token = res.token 
-            document.cookie =`jwt=${token}`
-            window.location = "/"
-        })
+            const token = res.token;
+            document.cookie =`jwt=${token}`;
+            window.location = "/";
+        });
 }
--- chronos-files/js/main.js
+++ chronos-files/js/main.js
@@ -9,103 +9,102 @@ function add_schedule(start, duration, week_day, type) {
     duration: duration,
     week_day: week_day,
     type: type 
-  })
+  });
 }
 
 function render_schedules() {
   for (let s of state.schedules) {
-    const schedule_height = document.querySelector(".schedules").offsetHeight
+    const schedule_height = document.querySelector(".schedules").offsetHeight;
 
-    const col = document.getElementById(s.week_day)
-    const schedules = col.querySelector(".schedules")
+    const col = document.getElementById(s.week_day);
+    const schedules = col.querySelector(".schedules");
 
-    const week_index = ['seg', 'ter', 'qua', 'qui', 'sex'].indexOf(s.week_day) 
-    console.log(week_index)
-    console.log("test")
+    const week_index = ['seg', 'ter', 'qua', 'qui', 'sex'].indexOf(s.week_day);
+    console.log(week_index);
+    console.log("test");
 
-    const el_top = s.start*schedule_height/11
+    const el_top = s.start*schedule_height/11;
     const el = `
                     <div style="height: ${s.duration*(schedule_height/11)}px; transform: translate(0,${el_top}px)" class="schedule-item">
 
                     </div>
-                    `
+                    `;
 
-    schedules.innerHTML += el
+    schedules.innerHTML += el;
   }
 }
 
 function handle_error(e) {
-	console.log(e)
+	console.log(e);
 }
 
 async function request_schedules(offset, forward=true) {
-	const api = "http://localhost:8080/api/time"
-	const today = new Date((new Date()).setDate((new Date().getDate()) + offset))
-	console.log(today)
-	const year = today.getFullYear().toString()
+	const api = "http://localhost:8080/api/time";
+	const today = new Date((new Date()).setDate((new Date().getDate()) + offset));
+	console.log(today);
+	const year = today.getFullYear().toString();
 	const month = (today.getMonth() + 1).toLocaleString('en-US', {
     minimumIntegerDigits: 2,
-    useGrouping: false })
+    useGrouping: false });
 
 
 	const day = today.getDate().toLocaleString('en-US', {
     minimumIntegerDigits: 2,
-    useGrouping: false })
+    useGrouping: false });
 
-	const week_day = today.getDay()
+	const week_day = today.getDay();
 
-	const week_day_name = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', "sab"][week_day]
+	const week_day_name = ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', "sab"][week_day];
 
-	const date = `${year}-${month}-${day}`
-	console.log(date)
+	const date = `${year}-${month}-${day}`;
+	console.log(date);
 
 	const req = await fetch(`${api}?date=${date}&page=0`, {
 		headers: {
 			"Authorization": "Bearer " + token()
 		}
-	}).catch(handle_error)
+	}).catch(handle_error);
 
 
-	const json = await req.json()
-	console.log(json)
+	const json = await req.json();
+	console.log(json);
 	
 	if (json.message)
-		window.location = "/login.html"
+		window.location = "/login.html";
 
 	json.forEach(t => {
-		const start = (new Date(t.start)).getHours() + (new Date(t.start)).getMinutes()/60 - 9
-		const end = (new Date(t.end)).getHours() + (new Date(t.end)).getMinutes()/60 - 9 
-		const duration = end - start
-		add_schedule(start,duration, week_day_name, "free")
-	})
+		const start = (new Date(t.start)).getHours() + (new Date(t.start)).getMinutes()/60 - 9;
+		const end = (new Date(t.end)).getHours() + (new Date(t.end)).getMinutes()/60 - 9;
+		const duration = end - start;
+		add_schedule(start,duration, week_day_name, "free");
+	});
 
 	if (week_day_name !=="sex" && forward)
-		await request_schedules(offset + 1, forward)
+		await request_schedules(offset + 1, forward);
 	if (week_day_name !== "seg" && !forward)
-		await request_schedules(offset - 1, forward)
-
+		await request_schedules(offset - 1, forward);
 }
 
 async function change_week(el, n) {
-	document.querySelectorAll(".week-selction button").forEach(el => el.classList.remove("current-week"))
-	el.classList.add("current-week")
+	document.querySelectorAll(".week-selction button").forEach(el => el.classList.remove("current-week"));
+	el.classList.add("current-week");
 
-	document.querySelectorAll(".schedule-item").forEach(e => e.remove())
+	document.querySelectorAll(".schedule-item").forEach(e => e.remove());
 
-	state.schedules = []
+	state.schedules = [];
 
-	await request_schedules(7*n, true)
-	await request_schedules(7*n, false)
-	render_schedules()
+	await request_schedules(7*n, true);
+	await request_schedules(7*n, false);
+	render_schedules();
 
 }
 
 
 (async function() {
 
-await request_schedules(0, true)
-await request_schedules(0, false)
-console.log(state)
+await request_schedules(0, true);
+await request_schedules(0, false);
+console.log(state);
 
-render_schedules()
-})()
+render_schedules();
+})();
