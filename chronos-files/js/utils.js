58715eeec365908d1161ecffe547782be1c70cd6
João Vitor Espig <jotaespig@gmail.com>
2023-11-28 13:15:07 -0300

    feat(utils.js): add redirect function

--- chronos-files/js/utils.js
+++ chronos-files/js/utils.js
@@ -1,6 +1,10 @@
 function token() {
+	const cookies = document.cookie.split(";").map(x => x.split('='));
+	const arr = cookies.find(x => x[0] == "jwt");
+    const token = arr ? arr[1] : "";
+	return token;
+}
 
-	const cookies = document.cookie.split(";").map(x => x.split('='))
-	const token = cookies.find(x => x[0] == "jwt")[1]
-	return token
+function redirect(location) {
+    window.location = location;
 }
