2e19e5a166c7fce9504d31c7a9fcaacdf68432ae
João Vitor Espig <jotaespig@gmail.com>
2023-12-05 13:40:52 -0300

    improved UX in login and signup

--- chronos-files/js/login.js
+++ chronos-files/js/login.js
@@ -14,7 +14,21 @@ function login(e) {
 	}).then(res => res.json())
         .then(res => {
             if (res.message) {
-                alert("failed: " + res.message);
+                document.getElementById("form").reset();
+                document.getElementById("username").focus();
+
+                Swal.fire({
+                    position: "top-end",
+                    title: "Erro",
+                    icon: "error",
+                    text: "O usuário e/ou a senha estão incorretos",
+                    width: 400,
+                    showConfirmButton: false,
+                    timer: 1500
+                });
+                setInterval(() => {
+                    document.getElementById("error").innerHTML = "O usuário e/ou a senha estão incorretos";
+                }, 1500);
                 return;
             }
             const token = res.token;
